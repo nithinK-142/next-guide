4f3d96bce38e55117765f218493c8d1c871941bc
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-26 10:43:45 +0530

    add: components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Search from "@/components/Search";
+
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Next Dynamic Widgets</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="pb-6 text-2xl font-semibold">Next Dynamic Widgets</h1>
+      <Search />
     </main>
   )
 }
---
+++ components/Search.tsx
@@ -0,0 +1,16 @@
+import Calculator from "./Calculator";
+import Stocks from "./Stocks";
+import Weather from "./Weather";
+
+const Search = () => {
+  return (
+    <div>
+      Search
+      <Calculator />
+      <Weather />
+      <Stocks />
+    </div>
+  );
+};
+
+export default Search;
