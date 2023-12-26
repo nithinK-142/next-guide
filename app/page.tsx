158f4cf880a4a4b19c2724a158242516697413c2
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-26 17:40:13 +0530

    add: Hero.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-semibold">Next Dark Mode</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <Hero />
     </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="w-[46rem] p-4 mt-12">
+      <div className="flex flex-col sm:flex-row items-center">
+        <div className="border-2 border-black dark:border-slate-600 rounded-full">
+          <Image src="/git.png" alt="git" width={200} height={200} />
+        </div>
+        <h1 className="text-[2.5rem] font-medium pl-12 flex flex-col items-center">
+          <span className="whitespace-nowrap">Hello and Welcome</span>
+          <span className="block">👋 I'm Lorem.</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
