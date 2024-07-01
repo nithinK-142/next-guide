79980265a688f18ba1e8cfb0acbd3d97fcbbb3ef
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-01 14:59:31 +0530

    update: api endpoint.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import BranchList from "./BranchList";
 export default function Home() {
   return (
     <main className="flex flex-col items-center text-center">
-      <h1 className="text-center text-2xl p-24">NextJs Guide</h1>
+      <h1 className="text-center text-2xl my-20">NextJs Guide</h1>
       <BranchList />
     </main>
   );
