3bededa82d3f6bd4e94b76f3d6cb2092df7a9010
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-04 17:07:57 +0530

    add: static app.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
-import BranchList from "./BranchList";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+import { data } from "./_data";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center text-center">
-      <h1 className="text-center text-2xl my-20">NextJs Guide</h1>
-      <BranchList />
+      <h1 className="text-center text-2xl my-20 tracking-widest group">
+        <span className="text-4xl inline-block rotate-12 group-hover:rotate-0 transition-transform duration-300">
+          A
+        </span>
+        <span className="inline-block rotate-0 group-hover:rotate-12 transition-transform duration-300">
+          NIME
+        </span>
+        <span className="text-4xl inline-block rotate-12 group-hover:rotate-0 transition-transform duration-300">
+          L
+        </span>
+        <span className="inline-block rotate-0 group-hover:rotate-12 transition-transform duration-300">
+          IST
+        </span>
+      </h1>
+
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((anime: AnimeProp, index) => (
+          <AnimeCard key={anime.id} anime={anime} index={index} />
+        ))}
+      </section>
     </main>
   );
 }
